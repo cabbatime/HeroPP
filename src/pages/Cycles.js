f4a1c9a3281dc8f2e99264fa2bda841914f03221
Cameron Halkett <cameronhalkett@Camerons-MacBook-Pro-2.local>
2024-06-30 22:22:43 +0100

    ideas status and delivery board10

--- src/pages/Cycles.js
+++ src/pages/Cycles.js
@@ -166,7 +166,7 @@ function Cycles() {
       </header>
       <div className="mt-4 flex">
         <aside className="w-1/4 pr-4">
-          <h2 className="text-xl font-semibold mb-4">Cycles</h2>
+          <h2 className="text-xl font-semibold text-gray-300 mb-4">Cycles</h2>
           <ul>
             <li
               className={`cursor-pointer mb-2 ${selectedCycle === 'All' ? 'text-blue-500' : 'text-gray-300'}`}
